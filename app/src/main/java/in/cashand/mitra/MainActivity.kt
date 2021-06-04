0e47830950c28bcc28175c94fa3d0e7cc25f1958
Ruhi <surveruhi@gmail.com>
2021-06-04 17:46:24 +0530

    Register page first commit

--- app/src/main/java/in/cashand/mitra/MainActivity.kt
+++ app/src/main/java/in/cashand/mitra/MainActivity.kt
@@ -1,11 +1,20 @@
 package `in`.cashand.mitra
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tv_register = findViewById<TextView>(R.id.tv_register)
+        tv_register.setOnClickListener(View.OnClickListener {
+            val register = Intent(this, RegisterActivity::class.java)
+            startActivity(register)
+        })
     }
 }
